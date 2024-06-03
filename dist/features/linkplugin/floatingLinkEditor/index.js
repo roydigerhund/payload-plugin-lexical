ddf3ca1bd731956dc808c7ec98ffc9ad3aaf782c
Matthias Martin <github@stichwort-m.de>
2024-06-03 22:35:30 +0200

    add gurucards to links

--- dist/features/linkplugin/floatingLinkEditor/index.js
+++ dist/features/linkplugin/floatingLinkEditor/index.js
@@ -83,7 +83,7 @@ function LinkEditor({ editor, isLink, setIsLink, anchorElem, }) {
                 if ('name' in f && f.name === 'doc') {
                     return Object.assign(Object.assign({}, f), { 
                         // TODO make this configurable
-                        relationTo: ['articles', 'pages', 'categories'] });
+                        relationTo: ['articles', 'pages', 'categories', 'gurucards'] });
                 }
                 return f;
             }).filter(f => !('name' in f) || f.name !== 'newTab')];
--- src/features/linkplugin/floatingLinkEditor/index.tsx
+++ src/features/linkplugin/floatingLinkEditor/index.tsx
@@ -86,7 +86,7 @@ function LinkEditor({
         return  {
           ...f,
           // TODO make this configurable
-          relationTo: ['articles', 'pages', 'categories']
+          relationTo: ['articles', 'pages', 'categories', 'gurucards']
         } as Field;
       }
       return f;
